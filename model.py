17a5e04bea57c3fa589d573fef4cdf07305559f4
vagrant <vagrant@vagrant.vm>
2017-08-07 19:33:10 +0000

    made a requirements text and laid out the skeletons for the models

---
+++ model.py
@@ -0,0 +1,41 @@
+"""Models and database functions for SF_Art project""" 
+
+from flask_sqlalchemy import SQLAlchmey 
+
+db = SQLAlchmey()
+
+
+################################################################################
+#Model definitions 
+
+class Artist(db.Model)
+    pass
+
+
+class Artpiece(db.Model)
+    pass
+
+
+class Decade(db.Model)
+    pass
+
+
+class Creditline(db.Model)
+    pass
+
+
+class ArtistArtpiece(db.Model)
+    pass
+
+
+class Medium(db.Model)
+    pass
+
+
+class UserArtpiece(db.Model)
+    pass
+
+
+class User(db.Model)
+    pass
+    
