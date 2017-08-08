1ce4a36d43a0126f8f1b54b5ef9e81bee3839c58
vagrant <vagrant@vagrant.vm>
2017-08-08 22:38:13 +0000

    created and defined load_artists()

--- seed.py
+++ seed.py
@@ -18,3 +18,16 @@ if response.status_code == 200:
     sf_data = response.json()
 
 ################################################################################
+def load_artist(): 
+    """Load artists from sf_data into database.""" 
+
+    print "Artists"
+
+    sf_data_len = len(sf_data)
+    for i in range(sf_data_len): 
+        artist = sf_data[i]['artist']
+        lname, fname = artist.split(",")
+        artist = Artist(lname=lname, fname=fname)
+        db.session.add(artist)
+
+    db.session.commit()
