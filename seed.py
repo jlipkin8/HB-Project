eea07afe818be9b964b76115eb705a4be23d0ae3
Josie Lipkin <jlipkin8@gmail.com>
2017-08-11 17:04:42 -0700

    re-named new_seed.py to seed.py

--- seed.py
+++ seed.py
@@ -1,9 +1,11 @@
 """Utility file to seed arts database from sfdata website """
 
 import requests 
-from sqlalchemy import fun 
+from sqlalchemy import func
 
-from model import * 
+from model import *
+import ast
+
 
 ################################################################################
 """Get art data from SF data """
@@ -18,66 +20,156 @@ if response.status_code == 200:
     sf_data = response.json()
 
 ################################################################################
-DATA_LEN = len(sf_data)
+# DATA_LEN = len(sf_data)
 
-def load_artists(): 
-    """Load artists from sf_data into database.""" 
+# create_timeperiods()
 
-    print "Artists"
+# for i in range(DATA_LEN):
+#     # load one row in Artist 
+#     load_artist(sf_data[i])
 
-    # might want to re-think splitting up the name
-    # might be harder to account for Uniqueness
-    # for i in range(DATA_LEN): 
-    #     artist = sf_data[i]['artist']
-    #     lname, fname = artist.split(",")
-    #     artist = Artist(lname=lname, fname=fname)
-    #     db.session.add(artist)
+#     #load one row in Artpiece 
+#     load_artpiece(sf_data[i])
 
-    for i in range(DATA_LEN): 
-        artist = sf_data[i]['artist']
+#     #load one row in Creditline 
+#     load_creditline(sf_data[i])
+
+#     #load obe row in ArtistArtpiece
+#     load_artist_artpiece(sf_data[i])
+
+#     #load one row in media
+#     load_medium(sf_data[i])
+# # end of for-loop 
+
+################################################################################
+# function definitions
+
+
+artists = []
+def load_artist(sf_datum): 
+    """Load artist from sf_data into database.""" 
+    print "made it into load_artists"
+
+    name = sf_datum.get('artist', '')
+    if (name not in artists) and (name): 
+        artists.append(name)
         artist = Artist(name=name)
         db.session.add(artist)
 
     db.session.commit()
 
 
-def load_artpieces(): 
-    """Load artpieces from sf_data into database""" 
+untitled_count = 1; 
+def load_artpiece(sf_datum):
+    """Load artpiece from sf_data into database."""
 
-    print "Artpieces"
-
-    for i in range(DATA_LEN): 
-        #get artist id 
-        artist_name = sf_data[i]['artist']
+    #get artist id 
+    artist_name = sf_datum.get('artist', '')
+    if artist_name:  
         artist = Artist.query.filter(Artist.name == artist_name).first()
-        artist_id = artist["artist_id"]
+        artist_id = artist.artist_id
 
-        #get timeperiod id
-        timeperiod = sf_data[i].get('created_at', None)
-        if timeperiod is not None: 
-            timeperiod = timeperiod[:4]
-            timeperiod = int(timeperiod)
+    #get timeperiod id
+    created_at = sf_datum.get('created_at', '')
+    if created_at: 
+        time_period = int(created_at[:4])
+        time_query =  Timeperiod.query.filter(((Timeperiod.start_period < time_period)
+                                            | (Timeperiod.start_period == time_period))
+                                            & ((Timeperiod.end_period >  time_period)
+                                            | (Timeperiod.end_period == time_period)))
+        returned_time_period= time_query.first()
+        tperiod_id = returned_time_period.timeperiod_id
+    else: 
+        tperiod_id = None
 
-        tperiod_query =  Timeperiod.query.filter((Timeperiod.start_period <= timeperiod) 
-                                                  & (Timeperiod.end_period >= timeperiod))
-        tperiod = tperiod_query.first()
-        toeruid_id = tperiod['timeperiod_id']
+    #get medium id 
+    medium = sf_datum.get('medium', '')
+    #maybe there is an id associated with None
 
-        #get medium id 
-        medium = sf_data[i].get('medium', None)
-        #maybe there is an id associated with None
-        if medium is not None: 
-            medium_query = Medium.query.filter(Medium.medium_desc == medium)
-            medium_object = medium_query.first() 
-            medium_id = medium_object['medium_id']
+    if medium: 
+        medium_query = Medium.query.filter(Medium.medium_desc == medium)
+        medium_object = medium_query.first() 
+        print medium_object
+        mediumid = medium_object.medium_id
+    else: 
+        mediumid = None
 
-        #get dimensions 
+    #get dimensions
+    # dimensions = sf_datum['geometry']['coordinates']
+    # print "\n"
+    dimensions = sf_datum['geometry']
+    geo_dict = ast.literal_eval(dimensions)
+    coordinates = geo_dict['coordinates']
 
-        #get location desc 
+    #get location desc
+    loc_desc = sf_datum.get('location_description', '')
 
-        #get title 
+    #get title 
+    title = sf_datum.get('title', '')
 
-        #get creditline
+    #get creditline
+    creditline = sf_datum.get('credit_line', '')
+    if creditline: 
+        credit_line= Creditline.query.filter(Creditline.creditline_name == creditline).first()
+        creditline_id = credit_line.creditline_id
+
+    else: 
+        creditline_id = None
+
+    # create artpiece instance 
+    artpiece = Artpiece(artist_id=artist_id,
+                        timeperiod_id=tperiod_id, 
+                        medium_id=mediumid, 
+                        dimensions=coordinates, 
+                        loc_desc=loc_desc, 
+                        title=title, 
+                        creditline_id=creditline_id)
+    db.session.add(artpiece)
+    db.session.commit()
+
+
+    # Get the Max user_id in the database
+    result = db.session.query(func.max(Artpiece.art_id)).one()
+    max_id = int(result[0])
+    load_artist_artpiece(artist_id, max_id)
+
+
+
+creditlines = []
+def load_creditline(sf_datum):
+    """Load creditline from sf_data into database."""
+
+    creditline_name = sf_datum.get('credit_line', '')
+    if creditline_name not in creditlines and creditline_name:
+        creditlines.append(creditline_name)  
+        creditline = Creditline(creditline_name=creditline_name)
+        db.session.add(creditline)
+
+    db.session.commit()
+
+
+#thinking about putting this load function inside the load_artpiece on 
+def load_artist_artpiece(artist_id, art_id): 
+    """Load artist and artpiece from sf_data into database"""
+
+    artistartpiece = ArtistArtPiece(artist_id=artist_id, art_id=art_id)
+    db.session.add(artistartpiece)
+
+    db.session.commit()
+
+
+media = []
+def load_medium(sf_datum): 
+    """Load medium from sf_data into database."""
+
+    medium = sf_datum.get('medium', '') 
+    if medium and medium not in media:
+            media.append(medium)
+            medium = Medium(medium_desc=medium)
+            db.session.add(medium)
+
+    db.session.commit()
+
 
 
 def create_timeperiods(): 
@@ -101,30 +193,27 @@ def create_timeperiods():
     db.session.commit()
 
 
-def load_creditlines(): 
-    """Load creditlines from sf_data into database.""" 
+################################################################################
 
-    print "Creditlines"
+if __name__ == "__main__": 
+    app = Flask(__name__)
+    connect_to_db(app)
+    db.create_all()
 
-    for i in range(DATA_LEN): 
-        creditline_name = sf_data[i]['credit_line']
-        if creditline_name: 
-            creditline = Creditline(creditline_name=creditline_name)
-            db.session.add(creditline)
+    # seeding tables with example data 
+    example_data = sf_data[10:21]
+    EX_LEN = len(example_data)
 
-    db.session.commit()
+    create_timeperiods()
+
+    for i in range(EX_LEN): 
+        load_artist(example_data[i])
+        load_creditline(example_data[i])
+        load_medium(example_data[i])
+        load_artpiece(example_data[i])
+
+
+    #testing out create_timeperiods 
     
 
-def load_media(): 
-    """Load media from sf_data into database. """ 
-
-    print "Media"
-
-    for i in range(DATA_LEN): 
-        medium = sf_data[i]['medium']
-        if medium: 
-            medium = Medium(medium_desc=medium)
-            db.session.add(medium)
-
-    db.session.commit()
 
