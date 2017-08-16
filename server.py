0ce04cb1b56fd28367b1a9df23d9459eeea33678
vagrant <vagrant@vagrant.vm>
2017-08-16 23:12:24 +0000

    added markers to the map with fake data

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
@@ -8,6 +8,14 @@ def homepage():
 
     return render_template("home_map.html")
 
+
+@app.route("/art-pieces")
+def artpiece_info():
+    """JSON info about artpieces"""
+    return jsonify({'markers':[{"name": "Mural", "lat": 37.7478586, "lng": -122.4556651}, 
+                   {"name": "Sculpture", "lat": 37.7419071, "lng": -122.4650348}, 
+                   {"name": "Drawing", "lat": 37.8085303, "lng": -122.4120591}]})
+    
 
 if __name__ == "__main__": 
     app.run(host="0.0.0.0")
